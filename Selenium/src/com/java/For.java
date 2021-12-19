7c312206810ce4782264a70050d9843a9f4bf434
Guru424 <guru.c142@gmail.com>
2021-12-19 17:03:35 +0530

    second change

--- Selenium/src/com/java/For.java
+++ Selenium/src/com/java/For.java
@@ -2,8 +2,12 @@ package com.java;
 
 public class For {
 public static void main(String[] args) {
-	
+	//Creating new for loop
 	for(int i=20;i>10;i--){
+		System.out.println("Value of i is ="+i);
+	}
+
+for(int i=20;i>10;i--){
 		System.out.println("Value of i is ="+i);
 	}
 }
